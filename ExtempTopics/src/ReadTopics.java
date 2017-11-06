bf0c8718167ad4c4158d04edd914427124f6a528
Kanishka Ragula <k.ragula1@gmail.com>
2017-11-06 13:01:54 -0700

    Add files via upload

--- ExtempTopics/src/ReadTopics.java
+++ ExtempTopics/src/ReadTopics.java
@@ -1,7 +1,81 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ReadTopics {
-	private FileReader input;
-	public static void main(String[] args){
+	
+	//Values for the class
+	private int ticker = 0;
+	private String[] questions, rquestions;
+	private ArrayList<Integer> nums;
+
+	public void init(String file) {
+		//Create an array to return later on.  Initialized here just in case file error needs to be described.
+		rquestions = new String[3];
 		
+		/**Try BufferedReader.  If file doesn't exist, will throw an error and given accordingly in rquestions.
+		 * 
+		 */
+		try {
+			BufferedReader questionReader = new BufferedReader(new FileReader("bin//" + file + ".txt"));
+			String str = null;
+			List<String> list = new ArrayList<String>();
+			
+			//Reads the file
+			while ((str = questionReader.readLine()) != null) {
+				list.add(str);
+
+			}
+			
+			//Converts the ArrayList to a String Array
+			questions = list.toArray(new String[list.size()]);
+			
+			//Creates a randomized list of numbers and assignment values.
+			nums = new ArrayList<Integer>(list.size());
+			for (int i = 0; i < list.size(); i++) {
+				nums.add(i);
+			}
+			Collections.shuffle(nums);
+			
+			//Get the first set of questions
+			rquestions[0] = questions[nums.get(0)];
+			rquestions[1] = questions[nums.get(1)];
+			rquestions[2] = questions[nums.get(2)];
+
+		} catch (IOException ie) {
+			//If file cannot be found.
+			rquestions = new String[3];
+			rquestions[0] = "Error";
+			rquestions[1] = "Reading";
+			rquestions[2] = "File";
+		}
 	}
+	
+	/**Method to return three questions from the list.  Calling consecutively will return different
+	 *  questions.  Once all questions have been exhausted, question list shuffled
+	 * @return Three random questions
+	 */
+	public String[] request() {
+		
+		//If the question list has been exhausted, reset and reshuffle
+		if(nums.size() == 0) {
+			return rquestions;
+		} else if(ticker+2>=nums.size()) {
+			ticker = 0;
+			Collections.shuffle(nums);
+		}
+		
+		//Set the values to be returned
+		rquestions[0] = questions[nums.get(ticker)];
+		rquestions[1] = questions[nums.get(ticker+1)];
+		rquestions[2] = questions[nums.get(ticker+2)];
+		ticker+=3;
+		
+		
+		return rquestions;
+	}
+
 }
--- ExtempTopics/src/TopicGUIFile.java
+++ ExtempTopics/src/TopicGUIFile.java
@@ -1,44 +1,130 @@
+import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.*;
-import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 public class TopicGUIFile {
+	
+	private ReadTopics fileRead;
 	private JFrame entry, mainScr;
 	private JComboBox choices;
-	private JButton loadMain, loadTopics;
-	private String[] qOptions = {"Domestic","International","Both"};
+	private JButton retEnt, loadTopics, loadNewTopics;
 	private JLabel headerEnt, headerMain;
+	private String[] questions;
+	private JPanel qPanel;
 	
+	public static void main(String[] args) {
+		TopicGUIFile gui = new TopicGUIFile();
+	}
+	
+	/**Main constructor.  Creates an instance of the class and displays the UI
+	 * 
+	 */
 	public TopicGUIFile(){
 		setup();
 		prepareGUI();
 	}
 	
+	/**Display's the GUI.  Has to be run after setup.
+	 * 
+	 */
 	private void prepareGUI(){
-		
+		entry.setVisible(true);
 	}
 	
+	/**Sets up every aspect of the user interface.  
+	 * 
+	 */
 	private void setup(){
+		
+		//Initialize different panels and arrays
+		final String[] areas = {"Combined", "National", "Foreign"};
+		fileRead = new ReadTopics();
+		JPanel temp = new JPanel();
+		JPanel temp2 = new JPanel();
+		questions = new String[3];
+		qPanel = new JPanel();
+		
+		//Setup of the header for the entry sccreen
+		headerEnt = new JLabel("Choose a Topic Area");
+		headerEnt.setHorizontalAlignment(JLabel.CENTER);
+		headerEnt.setFont(new Font("Calibri", Font.PLAIN, 23));
+		
+		//Set up the entry window
 		entry = new JFrame("Topic Area");
+		entry.setSize(200, 300);
+		entry.setLayout(new GridLayout(4,1));
+		entry.setResizable(false);
+		entry.add(headerEnt);
 		entry.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		mainScr = new JFrame("Topics");
-		mainScr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//JCombo Box for selecting area
+		choices = new JComboBox(areas);
+		temp.add(choices);
+		entry.add(temp);
 		
-		loadMain = new JButton("Choose Area");
-		loadMain.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
+		//Button to accept selected value and call appropriate file
+		loadTopics = new JButton("Load Questions");
+		loadTopics.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
 				entry.dispose();
 				mainScr.setVisible(true);
+				fileRead.init((String) choices.getSelectedItem());
+				String[] temp = fileRead.request();
+				//Display value of questions when main screen is rendered
+				qPanel.add(new JLabel("<html>"+temp[0] + "<br>"+temp[1] + "<br>"+temp[2] + "</html>"));
 			}
 		});
+		temp2.add(loadTopics);
+		entry.add(temp2);
 		
 		
-		loadTopics = new JButton("Get Topics");
-		loadTopics.addActionListener(new ActionListener(){
+
+		//Set up main screen
+		mainScr = new JFrame("Topics");
+		mainScr.setLayout(new GridLayout(4,1));
+		mainScr.setSize(850, 400);
+		mainScr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainScr.setResizable(false);
+		
+		//Set up the header for the main screen
+		headerMain = new JLabel("Questions:");
+		headerMain.setHorizontalAlignment(JLabel.CENTER);
+		headerMain.setFont(new Font("Calibri", Font.PLAIN, 23));
+		
+		//Button to return back to entry
+		retEnt = new JButton("Return to Area Chooser");
+		retEnt.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
+				qPanel.removeAll();
+				mainScr.dispose();
+				entry.setVisible(true);
 				
 			}
 		});
+		
+		//Refresh the page to get three new unique questions
+		loadNewTopics = new JButton("Get New Topics");
+		loadNewTopics.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				String[] temp = fileRead.request();
+				qPanel.removeAll();
+				qPanel.add(new JLabel("<html>"+temp[0] + "<br>"+temp[1] + "<br>"+temp[2] + "</html>"));
+				qPanel.revalidate();
+				qPanel.repaint();
+			}
+		});
+		
+		
+		//Add final touches to main screen
+		mainScr.add(headerMain);
+		mainScr.add(qPanel);
+		mainScr.add(retEnt);
+		mainScr.add(loadNewTopics);
+		
 	}
 }
